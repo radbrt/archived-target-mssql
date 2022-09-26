f4a3159900d421ae4b562fbce7395ade08bd437f
Henning <henning.holgersen@gmail.com>
2022-09-26 20:28:53 +0200

    init

---
+++ target_mssql/sinks.py
@@ -0,0 +1,32 @@
+"""mssql target sink class, which handles writing streams."""
+
+from __future__ import annotations
+
+from singer_sdk.sinks import SQLConnector, SQLSink
+
+
+class mssqlConnector(SQLConnector):
+    """The connector for mssql.
+
+    This class handles all DDL and type conversions.
+    """
+
+    allow_column_add: bool = True  # Whether ADD COLUMN is supported.
+    allow_column_rename: bool = True  # Whether RENAME COLUMN is supported.
+    allow_column_alter: bool = False  # Whether altering column types is supported.
+    allow_merge_upsert: bool = False  # Whether MERGE UPSERT is supported.
+    allow_temp_tables: bool = True  # Whether temp tables are supported.
+
+    def get_sqlalchemy_url(self, config: dict) -> str:
+        """Generates a SQLAlchemy URL for mssql.
+
+        Args:
+            config: The configuration for the connector.
+        """
+        return f"mssql+pyodbc://{config['user']}:{config['password']}@{config['host']}:{config['port']}/{config['database']}?driver=ODBC+Driver+17+for+SQL+Server&schema={config['schema']}"
+
+
+class mssqlSink(SQLSink):
+    """mssql target sink class."""
+
+    connector_class = mssqlConnector
