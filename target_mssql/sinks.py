c1ab20751024f40e2eb9a7fcfe0f3ac93b103f95
Henning <henning.holgersen@gmail.com>
2022-10-04 20:27:37 +0200

    allow upsert and alter

--- target_mssql/sinks.py
+++ target_mssql/sinks.py
@@ -13,8 +13,8 @@ class mssqlConnector(SQLConnector):
 
     allow_column_add: bool = True  # Whether ADD COLUMN is supported.
     allow_column_rename: bool = True  # Whether RENAME COLUMN is supported.
-    allow_column_alter: bool = False  # Whether altering column types is supported.
-    allow_merge_upsert: bool = False  # Whether MERGE UPSERT is supported.
+    allow_column_alter: bool = True  # Whether altering column types is supported.
+    allow_merge_upsert: bool = True  # Whether MERGE UPSERT is supported.
     allow_temp_tables: bool = True  # Whether temp tables are supported.
 
 
