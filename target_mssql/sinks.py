6be1fc45cd2ef589491ac96f32b0d80f4668b209
Henning <henning.holgersen@gmail.com>
2022-10-26 21:22:06 +0200

    force types for numeric

--- target_mssql/sinks.py
+++ target_mssql/sinks.py
@@ -3,6 +3,13 @@
 from __future__ import annotations
 
 from singer_sdk.sinks import SQLConnector, SQLSink
+from typing import Any, Generic, Mapping, TypeVar, Union, cast
+from singer_sdk.helpers._typing import (
+    JSONSCHEMA_ANNOTATION_SECRET,
+    JSONSCHEMA_ANNOTATION_WRITEONLY,
+    append_type,
+    get_datelike_property_type,
+)
 import sqlalchemy
 
 class mssqlConnector(SQLConnector):
@@ -212,23 +219,65 @@ class mssqlConnector(SQLConnector):
                 f"on table '{full_table_name}'."
             ) from e
 
+    def _jsonschema_type_check(jsonschema_type: dict, type_check: tuple[str]) -> bool:
+        """Return True if the jsonschema_type supports the provided type.
+        Args:
+            jsonschema_type: The type dict.
+            type_check: A tuple of type strings to look for.
+        Returns:
+            True if the schema suports the type.
+        """
+        if "type" in jsonschema_type:
+            if isinstance(jsonschema_type["type"], (list, tuple)):
+                for t in jsonschema_type["type"]:
+                    if t in type_check:
+                        return True
+            else:
+                if jsonschema_type.get("type") in type_check:
+                    return True
 
-        # ddl = sqlalchemy.DDL(
-        #         "ALTER TABLE %(table)s ADD %(create_column)s",
-        #         {
-        #             "table": full_table_name,
-        #             "create_column": create_column_clause,
-        #         },
-        #     )
-        # print(ddl)
+        if any(t in type_check for t in jsonschema_type.get("anyOf", ())):
+            return True
 
-        # ddl.execute(self.connection)
+        return False
 
-        # cur = self.connection.cursor()
-        # cur.execute(ddl)
-        # self.connection.execute(
-        #     ddl
-        # )
+
+
+    @staticmethod
+    def to_sql_type(self, jsonschema_type: dict) -> sqlalchemy.types.TypeEngine:
+        """Convert JSON Schema type to a SQL type.
+        Args:
+            jsonschema_type: The JSON Schema object.
+        Returns:
+            The SQL type.
+        """
+        if self._jsonschema_type_check(jsonschema_type, ("string",)):
+            datelike_type = get_datelike_property_type(jsonschema_type)
+            if datelike_type:
+                if datelike_type == "date-time":
+                    return cast(sqlalchemy.types.TypeEngine, sqlalchemy.types.DATETIME())
+                if datelike_type in "time":
+                    return cast(sqlalchemy.types.TypeEngine, sqlalchemy.types.TIME())
+                if datelike_type == "date":
+                    return cast(sqlalchemy.types.TypeEngine, sqlalchemy.types.DATE())
+
+            maxlength = jsonschema_type.get("maxLength")
+            return cast(sqlalchemy.types.TypeEngine, sqlalchemy.types.VARCHAR(maxlength))
+
+        if self._jsonschema_type_check(jsonschema_type, ("integer",)):
+            return cast(sqlalchemy.types.TypeEngine, sqlalchemy.types.INTEGER())
+        if self._jsonschema_type_check(jsonschema_type, ("number",)):
+            return cast(sqlalchemy.types.TypeEngine, sqlalchemy.types.NUMERIC(22, 16))
+        if self._jsonschema_type_check(jsonschema_type, ("boolean",)):
+            return cast(sqlalchemy.types.TypeEngine, sqlalchemy.types.BOOLEAN())
+
+        if self._jsonschema_type_check(jsonschema_type, ("object",)):
+            return cast(sqlalchemy.types.TypeEngine, sqlalchemy.types.VARCHAR())
+
+        if self._jsonschema_type_check(jsonschema_type, ("array",)):
+            return cast(sqlalchemy.types.TypeEngine, sqlalchemy.types.VARCHAR())
+
+        return cast(sqlalchemy.types.TypeEngine, sqlalchemy.types.VARCHAR())
 
 
 
