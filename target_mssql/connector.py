8e974e97023a14c12bcc475200a40f6efd7a9561
Henning <henning.holgersen@gmail.com>
2022-10-31 18:55:23 +0100

    nvarchar 2000

--- target_mssql/connector.py
+++ target_mssql/connector.py
@@ -267,22 +267,22 @@ class mssqlConnector(SQLConnector):
                     return cast(sqlalchemy.types.TypeEngine, sqlalchemy.types.DATE())
 
             maxlength = jsonschema_type.get("maxLength")
-            return cast(sqlalchemy.types.TypeEngine, mssql.NVARCHAR())
+            return cast(sqlalchemy.types.TypeEngine, mssql.NVARCHAR(2000))
 
         if self._jsonschema_type_check(jsonschema_type, ("integer",)):
             return cast(sqlalchemy.types.TypeEngine, sqlalchemy.types.INTEGER())
         if self._jsonschema_type_check(jsonschema_type, ("number",)):
             return cast(sqlalchemy.types.TypeEngine, sqlalchemy.types.NUMERIC(22, 16))
         if self._jsonschema_type_check(jsonschema_type, ("boolean",)):
-            return cast(sqlalchemy.types.TypeEngine, mssql.VARCHAR())
+            return cast(sqlalchemy.types.TypeEngine, mssql.VARCHAR(2000))
 
         if self._jsonschema_type_check(jsonschema_type, ("object",)):
-            return cast(sqlalchemy.types.TypeEngine, mssql.NVARCHAR())
+            return cast(sqlalchemy.types.TypeEngine, mssql.NVARCHAR(2000))
 
         if self._jsonschema_type_check(jsonschema_type, ("array",)):
-            return cast(sqlalchemy.types.TypeEngine, mssql.NVARCHAR())
+            return cast(sqlalchemy.types.TypeEngine, mssql.NVARCHAR(2000))
 
-        return cast(sqlalchemy.types.TypeEngine, mssql.NVARCHAR())
+        return cast(sqlalchemy.types.TypeEngine, mssql.NVARCHAR(2000))
 
 
     def create_temp_table_from_table(self, from_table_name):
